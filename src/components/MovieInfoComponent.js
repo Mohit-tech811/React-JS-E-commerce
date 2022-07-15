6388381c027d51bd05bd980346cf395c112c4817
Mohit Thakur <mohit.thakur@compunneldigital.com>
2022-07-15 14:33:20 +0530

    first commit

---
+++ src/components/MovieInfoComponent.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 20px 30px;
+  justify-content: center;
+  border-bottom: 1px solid lightgray;
+`;
+const CoverImage = styled.img`
+  object-fit: cover;
+  height: 350px;
+`;
+const InfoColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+`;
+const MovieName = styled.span`
+  font-size: 22px;
+  font-weight: 600;
+  color: black;
+  margin: 15px 0;
+  white-space: nowrap;
+  overflow: hidden;
+  text-transform: capitalize;
+  text-overflow: ellipsis;
+  & span {
+    opacity: 0.8;
+  }
+`;
+const MovieInfo = styled.span`
+  font-size: 16px;
+  font-weight: 500;
+  color: black;
+  overflow: hidden;
+  margin: 4px 0;
+  text-transform: capitalize;
+  text-overflow: ellipsis;
+
+  & span {
+    opacity: 0.5;
+  }
+`;
+const Close = styled.span`
+  font-size: 16px;
+  font-weight: 600;
+  color: black;
+  background: lightgray;
+  height: fit-content;
+  padding: 8px;
+  border-radius: 50%;
+  cursor: pointer;
+  opacity: 0.8;
+`;
+const MovieInfoComponent = (props) => {
+  const [movieInfo, setMovieInfo] = useState();
+  const { selectedMovie } = props;
+
+  useEffect(() => {
+    Axios.get(
+      'https://localhost:44372/api/User/Movies',
+    ).then((response) => setMovieInfo(response.data));
+  }, [selectedMovie]);
+  return (
+    <Container>
+      {movieInfo ? (
+        <>
+          <CoverImage src={movieInfo?.imageURL} alt={movieInfo?.name} />
+          <InfoColumn>
+            <MovieName>
+              {movieInfo?.name}: <span>{movieInfo?.name}</span>
+            </MovieName>
+            <MovieInfo>
+              IMDB Rating: <span>{movieInfo?.id}</span>
+            </MovieInfo>
+            <MovieInfo>
+              price: <span>{movieInfo?.price}</span>
+            </MovieInfo>
+        
+          </InfoColumn>
+          <Close onClick={() => props.onMovieSelect()}>X</Close>
+        </>
+      ) : (
+        "Loading..."
+      )}
+    </Container>
+  );
+};
+export default MovieInfoComponent;
